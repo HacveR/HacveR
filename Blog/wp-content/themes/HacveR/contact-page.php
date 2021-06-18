407b1656ce712134aeb03bba25cb6d3f30e4f21e
HacveR <kamil.cybersec@gmail.com>
2021-06-18 12:45:56 +0100

    update - menu addet

---
+++ Blog/wp-content/themes/HacveR/contact-page.php
@@ -0,0 +1,25 @@
+
+    <?php
+       get_header();
+    ?>
+    <!-- profile picture, Linked in and social media links -->
+    <section class="hacver-blog top-space">
+        
+        <div class="post-odd">
+            contact page
+        </div>
+        
+        
+        <div class="post-even">
+            
+        </div>
+    </section>
+    
+   
+
+    <?php 
+        get_footer();
+    ?>
+
+
+
