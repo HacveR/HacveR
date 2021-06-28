5b776efc760337f23f9ae38bc32c995cc59389a2
HacveR <kamil.cybersec@gmail.com>
2021-06-28 09:26:45 +0100

    update

---
+++ Blog/wp-content/themes/HacveR/page-projects.php
@@ -0,0 +1,115 @@
+
+    <?php
+       get_header();
+    ?>
+    <!-- profile picture, Linked in and social media links -->
+    <section class="hacver-projects top-space row "> 
+        <aside class="hacver-projects-aside col-4" >
+        
+        	<div class="hacver-sidebar-projects">
+        	<h1>Sub title</h1>
+        		<ul>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        		</ul>
+        	</div>
+        	<div>
+        	<h1>Sub title</h1>
+        		<ul>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        			<li><a href="#">ul-li element list</a></li>
+        		</ul>
+        	</div>
+         </aside>
+        <section class="hacver-projects-projectlist  col-8">
+       
+             <!--  one of my latest projects  -->
+             <div class="home-latest-projects">
+                <h1><a href="#">project title</a></h1> 
+                <div class="row">
+                    <img class="project-icon col-3" src="<?php echo get_template_directory_uri()?>/assets/images/folder_icon_256x256.png" alt="project-icon"/> 
+                    <p class="col-10 project-desc row">Short description about project. short description about project. Short description about project.Short description about project. 
+                                    short description about project. Short description about project. Short description about project. short description about project. Short description about project.
+                    
+                    <span class="col-8 span-cat">technologies: html, css, php, sql</span> <span class="col-2 span-cat">Author: Kamil l</span> <span class="col-2 span-cat"> Date:2000/1/1</span></p>
+                </div>
+             </div>
+             <!-- HR separator --> 
+             <hr>
+             
+              <!--  one of my latest projects  -->
+             <div class="home-latest-projects">
+                <h1><a href="#">project title</a></h1> 
+                <div class="row">
+                    <img class="project-icon col-3" src="<?php echo get_template_directory_uri()?>/assets/images/folder_icon_256x256.png" alt="project-icon"/> 
+                    <p class="col-10 project-desc row">Short description about project. short description about project. Short description about project.Short description about project. 
+                                    short description about project. Short description about project. Short description about project. short description about project. Short description about project.
+                    
+                    <span class="col-8 span-cat">technologies: html, css, php, sql</span> <span class="col-2 span-cat">Author: Kamil l</span> <span class="col-2 span-cat"> Date:2000/1/1</span></p>
+                </div>
+             </div>
+             <!-- HR separator --> 
+             <hr>
+             
+              <!--  one of my latest projects  -->
+             <div class="home-latest-projects">
+                <h1><a href="#">project title</a></h1> 
+                <div class="row">
+                    <img class="project-icon col-3" src="<?php echo get_template_directory_uri()?>/assets/images/folder_icon_256x256.png" alt="project-icon"/> 
+                    <p class="col-10 project-desc row">Short description about project. short description about project. Short description about project.Short description about project. 
+                                    short description about project. Short description about project. Short description about project. short description about project. Short description about project.
+                    
+                    <span class="col-8 span-cat">technologies: html, css, php, sql</span> <span class="col-2 span-cat">Author: Kamil l</span> <span class="col-2 span-cat"> Date:2000/1/1</span></p>
+                </div>
+             </div>
+             <!-- HR separator --> 
+             <hr>
+             
+              <!--  one of my latest projects  -->
+             <div class="home-latest-projects">
+                <h1><a href="#">project title</a></h1> 
+                <div class="row">
+                    <img class="project-icon col-3" src="<?php echo get_template_directory_uri()?>/assets/images/folder_icon_256x256.png" alt="project-icon"/> 
+                    <p class="col-10 project-desc row">Short description about project. short description about project. Short description about project.Short description about project. 
+                                    short description about project. Short description about project. Short description about project. short description about project. Short description about project.
+                    
+                    <span class="col-8 span-cat">technologies: html, css, php, sql</span> <span class="col-2 span-cat">Author: Kamil l</span> <span class="col-2 span-cat"> Date:2000/1/1</span></p>
+                </div>
+             </div>
+             
+             <hr>
+             
+              <!--  one of my latest projects  -->
+             <div class="home-latest-projects">
+                <h1><a href="#">project title</a></h1> 
+                <div class="row">
+                    <img class="project-icon col-3" src="<?php echo get_template_directory_uri()?>/assets/images/folder_icon_256x256.png" alt="project-icon"/> 
+                    <p class="col-10 project-desc row">Short description about project. short description about project. Short description about project.Short description about project. 
+                                    short description about project. Short description about project. Short description about project. short description about project. Short description about project.
+                    
+                    <span class="col-8 span-cat">technologies: html, css, php, sql</span> <span class="col-2 span-cat">Author: Kamil l</span> <span class="col-2 span-cat"> Date:2000/1/1</span></p>
+                </div>
+             </div>
+            <!-- HR separator -->  
+        	<hr>
+        	
+    	</section>
+    </section>
+   
+
+    <?php 
+        get_footer();
+    ?>
+
+
+
